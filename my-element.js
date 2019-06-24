9df286ce8923c7030cfc3545cdc403319b8f43f2
shinmachi <d.shinmachi.aist@gmail.com>
2019-06-24 17:01:48 +0900

    1 file modified

--- my-element.js
+++ my-element.js
@@ -1,9 +1,35 @@
 import { LitElement, html } from 'lit-element';
 
 class MyElement extends LitElement {
+  static get properties() {
+    return {
+      myString: { type: String },
+      myArray: { type: Array },
+      myBool: { type: Boolean },
+      myInt: { type: Number }
+    };
+  }
+  constructor() {
+    super();
+    this.myString = 'Hello World';
+    this.myArray = ['an', 'array', 'of', 'test', 'data'];
+    this.myBool = true;
+    this.myInt = 10;
+  }
   render(){
     return html`
-      <p>A paragraph</p>
+      <p>${this.myString}</p>
+      <ul>
+        ${this.myArray.map(i => html`<li>${i}</li>` )}
+      </ul>
+      ${this.myBool?
+        html `<p>Render some HTML if myBool is true`:
+        html `<p>Render some other HTML if myBool is fales</p>`
+      }
+     ${(this.myInt == 11)?
+              html `<p>Render some HTML if myInt is 10`:
+        html `<p>Render some other HTML if myInt is other than 10</p>`
+     }
     `;
   }
 }
