352f73796ccb9cda9e21d22c1792901a8e6cb694
shinmachi <d.shinmachi.aist@gmail.com>
2019-06-25 19:03:06 +0900

    2 files modified

--- gtc-wc-external-style.js
+++ gtc-wc-external-style.js
@@ -2,7 +2,13 @@ import { LitElement, html, css } from 'lit-element';
 
 class GtcWcExternalStyle extends LitElement {
   static styles = css`
+    .externalList {
+      margin: 10px 0 0;
+      padding: 0;
+      font-size: 14px;
+      list-style-type: none;
+    }
   `;
 }
 
-custom
+customElements.define('gtc-wc-external-style', GtcWcExternalStyle)
